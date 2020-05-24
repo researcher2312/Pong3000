431062bd7a69bda0b81840e297157dc1220bee08
msegedyn <researcher2312@gmail.com>
2020-05-24 13:50:05 +0200

    added collision detect for the ball

--- Ball.cpp
+++ Ball.cpp
@@ -1,15 +1,45 @@
-#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
 #include "Ball.h"
 
-Ball::Ball()
+Ball::Ball(int x, int y, int speed_x, int speed_y):
+    speed_x(speed_x), speed_y(speed_y)
 {
-    setRect(0,0,100,100);
-    QTimer* timer = new QTimer();
+    setRect(x,y,50,50);
+    timer = new QTimer();
     connect(timer, SIGNAL(timeout()), this, SLOT(move()));
     timer->start(50);
 }
 
 void Ball::move()
 {
-    setPos(x()+2, y()+2);
+    detectWallCollision();
+    detectPaddleColision();
+    setPos(x()+speed_x, y()+speed_y);
+
+}
+
+bool Ball::detectWallCollision()
+{
+    if(y() <= 0 || y() >= scene()->height()-rect().height()){
+        speed_y = -speed_y;
+        return true;
+    }
+    if(x() <= 0 || x() >= scene()->width()-rect().width()){
+        speed_x = -speed_x;
+        return true;
+    }
+    return false;
+}
+
+bool Ball::detectPaddleColision()
+{
+    QList<QGraphicsItem*> colliding_items = collidingItems();
+    for (auto& tested: colliding_items){
+        if (typeid (*tested) == typeid (Paddle)){
+            speed_x = -speed_x;
+            return true;
+        }
+    }
+    return false;
 }
--- Ball.h
+++ Ball.h
@@ -3,11 +3,18 @@
 
 #include <QGraphicsRectItem>
 #include <QObject>
+#include <QTimer>
 
 class Ball: public QObject, public QGraphicsRectItem{
     Q_OBJECT
+private:
+    QTimer* timer;
+    int speed_x;
+    int speed_y;
+    bool detectWallCollision();
+    bool detectPaddleColision();
 public:
-    Ball();
+    Ball(int x=0, int y=0, int speed_x=5, int speed_y=5);
 public slots:
     void move();
 };
